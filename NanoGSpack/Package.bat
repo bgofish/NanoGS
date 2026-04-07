8e69e826101767ece35a8560599c984f46163223
bb6 <73919236+bgofish@users.noreply.github.com>
2026-04-07 17:09:15 +1200

    Add files via upload

---
+++ NanoGSpack/Package.bat
@@ -0,0 +1,13 @@
+"C:\Program Files\Epic Games\UE_5.6\Engine\Build\BatchFiles\RunUAT.bat" ^
+  BuildCookRun ^
+  -project="B:\UE56\UE56_Blank\UE56_Blank.uproject" ^
+  -noP4 ^
+  -platform=Win64 ^
+  -clientconfig=Shipping ^
+  -cook ^
+  -build ^
+  -stage ^
+  -pak ^
+  -archive ^
+  -archivedirectory="B:\UE56\Package"
+  
